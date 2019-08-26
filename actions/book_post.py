728d2b39fabee2a7ed865ecadcdc4d84843f5d46
Sanchit <sanchit1992k@gmail.com>
2019-08-26 12:38:59 +0000

    On branch master

--- actions/book_post.py
+++ actions/book_post.py
@@ -1,2 +1,22 @@
-hello
+import sys
+import json
+import requests 
+
+from st2common.runners.base_action import Action
+
+class MyAction(Action):
+	def run(self,id,title,descp,pgcount,excerpt,pubdate):
+		try:
+			data={"ID": id,"Title":title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate}
+			data1=json.dumps(data)
+			headers={'content-type': 'application/json'}
+			url='https://fakerestapi.azurewebsites.net/api/Books'
+			response=requests.post(url,headers=headers,data=data1,timeout=6.0)
+			print(response)
+			data2=response.json()
+			print(data2)
+			
+		except requests.exceptions.Timeout:
+                        print("Request timeout")
+                        sys.exit(0)
 
