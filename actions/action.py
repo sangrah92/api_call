86ccadec6d753c15756c56ccd1e727921b4c0e1b
sangrah92 <54179007+sangrah92@users.noreply.github.com>
2019-08-26 17:23:00 +0530

    Update action.py

--- actions/action.py
+++ actions/action.py
@@ -4,6 +4,13 @@ from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
     def run(self, url):
-        resp = req.get(url,timeout=6.0)
-        print(resp.status_code)
-	print(resp.url) 
+        try:
+		resp = req.get(url,timeout=6.0)
+		print(resp.status_code)
+		print(resp.url)
+	except req.exceptions.MissingSchema:
+                print("invalid URL")
+                sys.exit(0)
+	except req.exceptions.Timeout:
+                print("Request timeout")
+                sys.exit(0)
